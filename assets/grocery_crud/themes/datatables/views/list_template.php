d9a640610183677f196348a712dbf00c21a62e36
emo_noel10 <emo_noel10@yahoo.com>
2015-09-15 02:52:40 +0800

    Modified list_template.php

--- assets/grocery_crud/themes/datatables/views/list_template.php
+++ assets/grocery_crud/themes/datatables/views/list_template.php
@@ -133,7 +133,7 @@ if (!empty($actions)) {
                 </h3>
             </div>
             <div class="modal-content">
-                <iframe class="modal-body" id="addFrame" src="" style="zoom:0" width="100%" height="748px" frameborder="0"></iframe>
+                <iframe class="modal-body" id="addFrame" onload="setIframeHeight(this.id)" src="" style="zoom:0" width="100%" height="748px" frameborder="0"></iframe>
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-effect-ripple btn-primary" style="overflow: hidden; position: relative;">Save</button>
@@ -157,7 +157,7 @@ if (!empty($actions)) {
                 <br>
             </div>
             <div class="modal-content">
-                <iframe class="modal-body" id="editFrame" src="" style="zoom:0" width="100%" height="748px" frameborder="0"></iframe>
+                <iframe class="modal-body" id="editFrame" onload="setIframeHeight(this.id)" src="" style="zoom:0" width="100%" height="748px" frameborder="0"></iframe>
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-effect-ripple btn-primary" style="overflow: hidden; position: relative;">Save</button>
@@ -181,7 +181,7 @@ if (!empty($actions)) {
                 <br>
             </div>
             <div class="modal-content">
-                <iframe class="modal-body" id="viewFrame" src="" style="zoom:0" width="100%" height="748px" frameborder="0"></iframe>            
+                <iframe class="modal-body" id="viewFrame" onload="setIframeHeight(this.id)" src="" style="zoom:0" width="100%" height="748px" frameborder="0"></iframe>            
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-effect-ripple btn-primary" style="overflow: hidden; position: relative;">Save</button>
@@ -190,3 +190,26 @@ if (!empty($actions)) {
         </div>
     </div>
 </div>
+
+<script type="text/javascript">
+    function setIframeHeight(id) {
+        var ifrm = document.getElementById(id);
+        var doc = ifrm.contentDocument ? ifrm.contentDocument :
+                ifrm.contentWindow.document;
+        ifrm.style.visibility = 'hidden';
+        ifrm.style.height = "10px"; // reset to minimal height ...
+        // IE opt. for bing/msn needs a bit added or scrollbar appears
+        ifrm.style.height = getDocHeight(doc) + 4 + "px";
+        ifrm.style.visibility = 'visible';
+    }
+
+    function getDocHeight(doc) {
+        doc = doc || document;
+        // stackoverflow.com/questions/1145850/
+        var body = doc.body, html = doc.documentElement;
+        var height = Math.max(body.scrollHeight, body.offsetHeight,
+                html.clientHeight, html.scrollHeight, html.offsetHeight);
+        return height;
+    }
+    ;
+</script>
