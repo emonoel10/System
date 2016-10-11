ba8ca9c82bcf61ba53de7214493161489fb69739
emonoel10 <emo_noel10@yahoo.com>
2016-10-11 22:44:37 +0800

    save latest commit

--- application/controllers/AboutBarangay.php
+++ application/controllers/AboutBarangay.php
@@ -13,3 +13,18 @@ class AboutBarangay extends CI_Controller {
 	}
 
 }
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class AboutBarangay extends CI_Controller {
+
+	public function __construct() {
+		parent::__construct();
+	}
+
+	public function index() {
+		$this->load->view('Frontend/barangay');
+	}
+
+}
